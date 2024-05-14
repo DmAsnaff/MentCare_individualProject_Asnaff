5281b415b086829a67cbc8caca37a3ac5bcd5334
DmAsnaff <mail2asnaff@gmail.com>
2024-05-14 23:33:26 +0530

    First Commit

--- app/src/main/java/com/example/mentcare_individualproject_asnaff/Home.java
+++ app/src/main/java/com/example/mentcare_individualproject_asnaff/Home.java
@@ -2,15 +2,80 @@ package com.example.mentcare_individualproject_asnaff;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Home extends AppCompatActivity {
+
+    ImageButton homebutton,locationbutton,historybutton,profilebutton, carebutton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         getWindow().setStatusBarColor(Color.parseColor("#54434E"));
+
+        homebutton = findViewById(R.id.homebutton);
+        carebutton = findViewById(R.id.carebutton);
+        locationbutton = findViewById(R.id.locationbutton);
+        historybutton = findViewById(R.id.historybutton);
+        profilebutton = findViewById(R.id.profilebutton);
+
+
+
+        homebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        carebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+        locationbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+        historybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+        profilebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Home.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+
+
+
     }
 }
